0c21c2f461c2290d2a79667d5043a0cde47db2fc
Ew0345 <ew0345@users.noreply.github.com>
2026-05-06 18:50:01 -0400

    No longer remove Steam store links anymore
    I'm dumb and it took me however long this has been on github to notice :)

--- HideShopAndNitro.plugin.js
+++ HideShopAndNitro.plugin.js
@@ -4,12 +4,13 @@
  * @description Hides the Shop & Nitro tabs from the main discord page
  * @version 1.0
  */
+
 let removeTabs;
 module.exports = class HideShopAndNitro {
     start() {
         removeTabs = setInterval(() => {
             for (var i = 0; i < document.querySelectorAll('a').length; i++) {
-                if (document.querySelectorAll('a')[i].href.includes('/shop') || document.querySelectorAll('a')[i].href.includes('/store')) {
+                if (document.querySelectorAll('a')[i].href.includes('/shop') || document.querySelectorAll('a')[i].href.includes('/store') && !document.querySelectorAll('a')[i].href.includes("steampowered")) {
                     if (document.querySelectorAll('a')[i].style.display !== 'none') {
                         document.querySelectorAll('a')[i].style.display = 'none';
                     }
@@ -21,7 +22,7 @@ module.exports = class HideShopAndNitro {
     stop() {
         clearInterval(removeTabs);
         for (var i = 0; i < document.querySelectorAll('a').length; i++) {
-            if (document.querySelectorAll('a')[i].href.includes('/shop') || document.querySelectorAll('a')[i].href.includes('/store')) {
+            if (document.querySelectorAll('a')[i].href.includes('/shop') || document.querySelectorAll('a')[i].href.includes('/store') && !document.querySelectorAll('a')[i].href.includes("steampowered")) {
                 document.querySelectorAll('a')[i].style.display = '';
             }
         }
